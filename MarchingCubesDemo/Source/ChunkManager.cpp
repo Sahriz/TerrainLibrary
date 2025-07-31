36fda294509987c44f5ca4fc4881552f121883c7
Sahriz <94785672+Sahriz@users.noreply.github.com>
2025-07-31 19:12:23 +0200

    Added a single comment for the marching cubes demo.

--- MarchingCubesDemo/Source/ChunkManager.cpp
+++ MarchingCubesDemo/Source/ChunkManager.cpp
@@ -17,7 +17,11 @@ void ChunkManager::GenerateChunk(const glm::vec3& position) {
 				// Generate if not yet stored
 				if (_chunkMap.find(coord) == _chunkMap.end()) {
 					glm::vec3 offset = coord * glm::vec3(_width, _height, _depth);
-					_chunkMap[coord] = std::move(Core::CreateMarchingCubes3DMeshGPU(_width, _height, _depth, offset, false)); //
+					//Try generating the mesh with and without GPU to see the difference in speed! The function call is the same but the end of
+					//the function call is GPU for the gpu implementtion. Please do keep in mind the noise map is still using compute shaders
+					//even on the cpu implementation, so that is technically a speedup that should not be granted as a possitive for the CPU part
+					//of this code. 
+					_chunkMap[coord] = std::move(Core::CreateMarchingCubes3DMeshGPU(_width, _height, _depth, offset, false)); 
 				}
 			}
 		}
